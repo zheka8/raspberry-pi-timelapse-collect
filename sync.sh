f17534e1761b827191428f4867e5c669b92c6788
monster <a@a>
2021-01-15 14:35:41 -0500

    added loop to repeat commands on failure and added to logging

--- sync.sh
+++ sync.sh
@@ -2,9 +2,17 @@
 
 # log ip address
 ip_addr=`ip addr show wlan0 | awk '/inet / {print $2}'`
-echo `date +"%Y-%m-%d-%H-%M-%S"` $ip_addr >> camera.log
+echo `date +"%Y-%m-%d-%H-%M-%S"` Sync from $HOSTNAME  $ip_addr >> logs/camera.log
 
 # move files
-rclone move --exclude .gitkeep images gdrive:images_$HOSTNAME
-rclone move --exclude .gitkeep logs gdrive:logs_$HOSTNAME
+mv_images="rclone move --exclude .gitkeep images gdrive:images_$HOSTNAME"
+mv_logs="rclone move --exclude .gitkeep logs gdrive:logs_$HOSTNAME"
 
+# repeat  if fails
+eval $mv_images
+eval $mv_logs
+while [ $? -ne 0 ]; do
+	eval $mv_images
+	eval $mv_logs
+	echo "repeat"
+done
