222f44b4e1056457f638654224042179fc528a0f
monster <a@a>
2021-01-15 14:14:34 -0500

    added ip address logging

--- sync.sh
+++ sync.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+# log ip address
+ip_addr=`ip addr show wlan0 | awk '/inet / {print $2}'`
+echo `date +"%Y-%m-%d-%H-%M-%S"` $ip_addr >> camera.log
+
+# move files
 rclone move --exclude .gitkeep images gdrive:images_$HOSTNAME
 rclone move --exclude .gitkeep logs gdrive:logs_$HOSTNAME
+
