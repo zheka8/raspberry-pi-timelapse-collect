91a735b08993b763392756c12069b1e0aea43469
monster <a@a>
2021-01-16 12:33:14 -0500

    gets full path and uses it in rclone command

--- sync.sh
+++ sync.sh
@@ -4,9 +4,18 @@
 ip_addr=`ip addr show wlan0 | awk '/inet / {print $2}'`
 echo `date +"%Y-%m-%d-%H-%M-%S"` Sync from $HOSTNAME  $ip_addr >> logs/camera.log
 
+# setup absolute paths
+images_dir="images"
+log_dir="logs"
+ 
+full_path=$(realpath $0)
+dir_path=$(dirname $full_path)
+echo $dir_path
+
+
 # move files
-mv_images="rclone move --exclude .gitkeep images gdrive:images_$HOSTNAME"
-mv_logs="rclone move --exclude .gitkeep logs gdrive:logs_$HOSTNAME"
+mv_images="rclone move --exclude .gitkeep $dir_path/$images_dir gdrive:images_$HOSTNAME"
+mv_logs="rclone move --exclude .gitkeep $dir_path/$log_dir gdrive:logs_$HOSTNAME"
 
 # repeat  if fails
 eval $mv_images
